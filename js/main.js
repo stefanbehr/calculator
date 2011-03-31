7b5ea80ab0aee8ef356d25fdd3129b12780fd0ca
Stefan Behr <sbehr1@gmail.com>
2011-03-31 00:57:38 -0500

    Implemented main calculator functions.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-	var operand;
-	var operands = []
-	var operators;
+	var operand = '';
+	var operands = [];
+	var operators = [];
+	var expression = '';
 	
+	$("#digits .digit").click(function() {
+		var digit = $(this).text();
+		if (operand.length < 21 && (operand != '' || digit != "0")) {
+			operand += digit;
+		}
+		$("#display").text(operand);
+	});
 	
+	$("#operators .operator").click(function() {
+		if (operand != '') {
+			operands.push(operand);
+		}
+		operand = '';
+		
+		var operator = $(this).text();
+		
+		if (operator != "=") {
+			operators.push(operator);
+		} else {
+			operators.reverse();
+			operands.reverse();
+			expression = '';
+			while (operators.length != 0) {
+				expression += operands.pop() + operators.pop();
+			}
+			expression += operands.pop();
+			$("#display").text(eval(expression).toString());
+			operands = [];
+			operators = [];
+		}		
+	});
 });
