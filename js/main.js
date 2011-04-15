ffd4412e1bcce3cb87c71e26d068341ae1a01bd8
Peter Behr <pbehr@uchicago.edu>
2011-04-15 00:06:51 -0700

    Converted tabs to spaces. Added self as an author. I enjoy superficiality. First post. Etc. Etc.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 $(document).ready(function() {
-	var operand = '';
-	var operands = [];
-	var operators = [];
-	var resultOperandFlag = false;
-	
-	$(".digits .digit").click(function() {
-		if (resultOperandFlag) {
-			operand = '';
-			resultOperandFlag = false;
-		}
-		var digit = $(this).text();
-		if (operand.length < 23 && operand != '0') {
-			operand += digit;
-		}
-		$("#display").text(operand);
-	});
-	
-	$("#operators .operator").click(function() {
-		if (operand != '') {
-			operands.push(operand);
-			operand = '';
-			resultOperandFlag = false;
-			
-			var operator = $(this).text();
-			
-			if (operator != "=") {
-				operators.push(operator);
-			} else {
-				operators.reverse();
-				operands.reverse();
-				var expression = '';
-				while (operators.length != 0) {
-					expression += operands.pop() + operators.pop();
-				}
-				expression += operands.pop();
-				result = eval(expression).toString();
-				$("#display").text(result);
-				operand = result;
-				resultOperandFlag = true;
-			}			
-		}
-	});
-	
-	$("#clear").click(function() {
-		$("#display").text("");
-		operands = [];
-		operators = [];
-		operand = '';
-	});
+    var operand = '';
+    var operands = [];
+    var operators = [];
+    var resultOperandFlag = false;
+    
+    $(".digits .digit").click(function() {
+        if (resultOperandFlag) {
+            operand = '';
+            resultOperandFlag = false;
+        }
+        var digit = $(this).text();
+        if (operand.length < 23 && operand != '0') {
+            operand += digit;
+        }
+        $("#display").text(operand);
+    });
+    
+    $("#operators .operator").click(function() {
+        if (operand != '') {
+            operands.push(operand);
+            operand = '';
+            resultOperandFlag = false;
+            
+            var operator = $(this).text();
+            
+            if (operator != "=") {
+                operators.push(operator);
+            } else {
+                operators.reverse();
+                operands.reverse();
+                var expression = '';
+                while (operators.length != 0) {
+                    expression += operands.pop() + operators.pop();
+                }
+                expression += operands.pop();
+                result = eval(expression).toString();
+                $("#display").text(result);
+                operand = result;
+                resultOperandFlag = true;
+            }            
+        }
+    });
+    
+    $("#clear").click(function() {
+        $("#display").text("");
+        operands = [];
+        operators = [];
+        operand = '';
+    });
 });
